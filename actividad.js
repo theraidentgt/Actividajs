e02d26b936eca16ca2e77edeb0abbf2495c91697
theraidentgt <jdzipita@gmail.com>
2015-09-10 18:07:02 -0500

    actividad

---
+++ actividad.js
@@ -0,0 +1,61 @@
+1. 
+var nGalones=7;
+var litro= 3.7854;
+
+var galon=1;
+
+var res= nGalones*litro/galon;
+console.log("En 7 galones hay " +res+ " litros");
+
+2. 
+var f= 100;
+var c= (f-32)*5/9;
+cosole.log(f+" grados fahernhit equivalen a " +c+ " grados centigrados");
+
+var c=30;
+
+k=c+273.15;
+
+console.log(c+" grados centigrados equivalen a " +k+" grados kelvin");
+
+var k= 303;
+var c= k-273.15;
+console.log(k+" grados kelvin equivalen a "+c+" grados centigrados");
+
+3.
+var pesoTierra= 73;
+
+
+var gtierra=9.807;
+
+
+var gLuna= 1.622;
+
+
+var pLuna= pesoTierra*gLuna/gtierra;
+console.log("Mi peso en la luna es de " + pLuna+ " kilogramos");
+
+4.
+var pi=3.1416;
+
+var radio=5;
+var area=pi*(radio^2);
+
+
+console.log("el area de un circulo de radio 5 es " +area);
+
+var base=10;
+
+var altura=5;
+var area=base*altura/2;
+
+
+console.log("el area de un triangulo  " +area);
+
+
+var lado=4;
+
+var area=l^l;
+
+
+console.log("el area de un cuadrado es " +area);
